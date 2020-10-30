f4ffc60378c5b29afe5139a45e2178b509b723b1
Abhinn Krishn <abhinnk21@gmail.com>
2020-10-30 20:36:26 +0530

    v0.2.0
    Fixed Long quote problem, which was causing the page to scroll when a long quote was loaded.

--- Scripts/script.js
+++ Scripts/script.js
@@ -9,6 +9,7 @@ window.addEventListener("load", function () {
     )
       .then((response) => response.json())
       .then((data) => {
+        //TODO: make available for video when avaliable
         document.body.style.backgroundImage = `url("${data.url}")`;
         if (data.copyright != undefined)
           document.querySelector(
@@ -20,16 +21,21 @@ window.addEventListener("load", function () {
       .catch((err) => console.log("Something's wrong with NASA API"));
 
     // Quote API
-    fetch("https://api.quotable.io/random")
+      fetch("https://api.quotable.io/random")
       .then((response) => response.json())
       .then((data) => {
-        if (data.content != undefined) {
+        if (data.content != undefined && data.content.length <= 215) {
           document.querySelector(".quote").innerHTML = `${data.content}`;
           document.querySelector(".qauthor").innerHTML = `- ${data.author}`;
+          localStorage['myQuote'] = data.content;
+          localStorage['myAuthor'] = data.author;
+        } else {
+          document.querySelector(".quote").innerHTML = localStorage['myQuote'];
+          document.querySelector(".qauthor").innerHTML = "- " + localStorage['myAuthor'];
         }
       })
       .catch((err) => console.log("Something's wrong with Quote API"));
-
+      
     // Time
     var myVar = setInterval(myTimer, 1000);
 
